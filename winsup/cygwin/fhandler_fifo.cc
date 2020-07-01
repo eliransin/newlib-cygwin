01383c03254735a9cff9fd06ba875aef93c1b12d
Ken Brown <kbrown@cornell.edu>
2020-07-01 16:07:19 -0400

    Tweak 'avoid infinite loop' patch

--- winsup/cygwin/fhandler_fifo.cc
+++ winsup/cygwin/fhandler_fifo.cc
@@ -1460,7 +1460,7 @@ fhandler_fifo::close ()
       owner_unlock ();
       if (dec_nreaders () == 0)
 	ResetEvent (read_ready);
-      else if (find_new_owner)
+      else if (find_new_owner && !IsEventSignalled (owner_found_evt))
 	{
 	  /* As long as there are still readers open, we should be
 	     able to quickly find a new owner.  But we don't want to
@@ -1469,16 +1469,19 @@ fhandler_fifo::close ()
 	     decrementing shmem->_nreaders. */
 	  int retries = 1000;
 	  bool found = false;
-
-	  while (nreaders () > 0 && !found && retries-- > 0
-		 && !IsEventSignalled (owner_found_evt))
+	  while (nreaders () > 0 && !found && retries-- > 0)
 	    {
-	      owner_lock ();
-	      if (get_owner ()) /* We missed owner_found_evt? */
+	      if (WaitForSingleObject (owner_found_evt, 1) == WAIT_OBJECT_0)
 		found = true;
 	      else
-		owner_needed ();
-	      owner_unlock ();
+		{
+		  owner_lock ();
+		  if (get_owner ()) /* We missed owner_found_evt? */
+		    found = true;
+		  else
+		    owner_needed ();
+		  owner_unlock ();
+		}
 	    }
 	  debug_printf ("found = %d, retries = %d, nreaders = %d",
 			found, retries, nreaders ());
